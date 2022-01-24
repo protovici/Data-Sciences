e3f7582f745c59db623115dafcea7f86ef904b47
protovici <protovici@gmail.com>
2022-01-23 19:24:27 -0600

    Updates

--- code/chart2gif.py
+++ code/chart2gif.py
@@ -6,13 +6,13 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
-
 import matplotlib.pyplot as plt
 import gif
 
 # settings
 plt.style.use("seaborn")
 gif.options.matplotlib["dpi"] = 300
+print('SETUP IS COMPLETE')
 
 df = yf.download("FB", 
                  start="2019-01-01", 
@@ -25,3 +25,15 @@ def helper_plot_1(df, i):
     ax = df.plot(title="Tesla's stock price", legend=False, style="o--")
     ax.set_xlabel("")
     ax.set_ylabel("Price ($)")
+
+fb_df = df[["Adj Close"]].resample("M").last()
+
+frames = []
+for i in range(1, len(fb_df)):
+    frames.append(helper_plot_1(fb_df, i))
+
+gif.save(frames, "fb_stock_price.gif", 
+         duration=15, unit="s", 
+         between="startend")
+
+print('Chart_2_Gif IS COMPLETED')
