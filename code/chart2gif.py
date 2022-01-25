4b07e6f40ce6b0d73068c82b725b072a07576c8c
protovici <38801203+protovici@users.noreply.github.com>
2022-01-24 18:28:06 -0600

    Updates

--- code/chart2gif.py
+++ code/chart2gif.py
@@ -14,23 +14,32 @@ plt.style.use("seaborn")
 gif.options.matplotlib["dpi"] = 300
 print('SETUP IS COMPLETE')
 
-df = yf.download("FB", 
+df = yf.download(["TSLA", "TWTR", "FB", "AMZN", "AAPL"], 
                  start="2019-01-01", 
                  end="2021-12-31")
 
+
 @gif.frame
-def helper_plot_1(df, i):
+def helper_plot_2(df, i):
+    
     df = df.copy()
     df.iloc[i:] = np.nan
-    ax = df.plot(title="Tesla's stock price", legend=False, style="o--")
+    
+    ax = df.plot(title="Selected stocks' change of value")
     ax.set_xlabel("")
-    ax.set_ylabel("Price ($)")
+    
+    # move the legend below the plot
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0 + box.height * 0.1,
+                     box.width, box.height * 0.9])
+    ax.legend(loc="upper center", bbox_to_anchor=(0.5, -0.1),
+              fancybox=True, shadow=True, ncol=5)
 
 fb_df = df[["Adj Close"]].resample("M").last()
 
 frames = []
 for i in range(1, len(fb_df)):
-    frames.append(helper_plot_1(fb_df, i))
+    frames.append(helper_plot_2(fb_df, i))
 
 gif.save(frames, "fb_stock_price.gif", 
          duration=15, unit="s", 
